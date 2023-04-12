aed72050e9b2f35889f6d6258c25e123bc23822d
kwngmin <kwngmin.k@gmail.com>
2023-04-12 16:42:26 +0900

    home 화면 문구 수정

--- .history/src/components/Title_20230412163646.tsx
+++ .history/src/components/Title_20230412163646.tsx
@@ -5,8 +5,8 @@ type TitleData = {
 export default function Title({ titleData }: { titleData: TitleData }) {
   const { title, description } = titleData;
   return (
-    <div className="pt-14 pb-16 px-4 lg:px-8">
-      <h1 className="text-3xl md:text-4xl font-bold mb-2">{title}</h1>
+    <div className="py-14 px-4 lg:px-12 border bg-zinc-100">
+      <h1 className="text-3xl md:text-4xl font-bold mb-3">{title}</h1>
       <p className="text-black/50 break-keep">{description}</p>
     </div>
   );
--- .history/src/components/Title_20230412163746.tsx
+++ .history/src/components/Title_20230412163746.tsx
@@ -5,7 +5,7 @@ type TitleData = {
 export default function Title({ titleData }: { titleData: TitleData }) {
   const { title, description } = titleData;
   return (
-    <div className="pt-14 pb-16 px-4 lg:px-8">
+    <div className="pt-14 pb-16 px-4 lg:px-12 bg-zinc-50 rounded-2xl border mb-4">
       <h1 className="text-3xl md:text-4xl font-bold mb-2">{title}</h1>
       <p className="text-black/50 break-keep">{description}</p>
     </div>
